1a91394eee42fe8ca18320f1563a7d08dca46c33
liaowh <2930795265@qq.com>
2024-10-10 12:00:43 +0800

    feat: 完善回文

--- 链表/回文链表.js
+++ 链表/回文链表.js
@@ -49,3 +49,58 @@ var isPalindrome2 = function(head) {
     frontPointer = head
     return recursivelyCheck(head)
 };
+
+// 方法三：用快慢指针遍历
+/*
+1.找到前半部分链表的尾节点。
+2.反转后半部分链表。
+3.判断是否回文。
+4.恢复链表。
+5.返回结果。
+*/
+
+const reverseList = (head) => {
+    let prev = null;
+    let curr = head;
+    while(curr !== null) {
+        let nextTemp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nextTemp
+    }
+    return prev
+}
+
+const endOfFirstHalf = (head) => {
+    let fast = head
+    let slow = head
+    while(fast.next !== null && fast.next.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    return slow
+}
+
+const isPalindrome3 = (head) => {
+    if (head == null) return true
+    // 找到前半部分链表尾节点并反转后半部分链表
+    const firstHalfEnd = endOfFirstHalf(head)
+    const secondHalfStart = reverseList(firstHalfEnd.next)
+
+    // 判断是否回文
+    let p1 = head
+    let p2 = secondHalfStart
+    let result = true
+    while (result && p2 !== null) {
+        if (p1.val != p2.val) result = false
+        p1 = p1.next
+        p2 = p2.next
+    }
+    // 还原链表并返回结果
+    firstHalfEnd.next = reverseList(secondHalfStart)
+    return result
+}
+
+
+
+
